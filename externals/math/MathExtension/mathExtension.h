e977778b98e3875b933af3b3c025caead925fcb6
Souto-Naitou <146906423+Souto-Naitou@users.noreply.github.com>
2024-10-23 12:06:07 +0900

    実行できる状態に

--- externals/math/MathExtension/mathExtension.h
+++ externals/math/MathExtension/mathExtension.h
@@ -3,12 +3,11 @@
 
 #pragma once
 #include "../Vector3.h"
-#include "../Matrix4x4.h"
 
 float cotanf(float _x);
 float clamp(
-	float _x, 
-	float _min, 
+	float _x,
+	float _min,
 	float _max
 );
 int clamp(
@@ -17,6 +16,6 @@ int clamp(
 	int _max
 );
 float Lerp(float _x, float _y, float _t);
-Vector3 TransformNormal(const Vector3& _v, const Matrix4x4& _m);
+//Vector3 TransformNormal(const Vector3& _v, const Matrix4x4& _m);
 Vector3 Lerp(const Vector3& _vx, const Vector3& _vy, float _t);
 Vector3 Slerp(const Vector3& _vx, const Vector3& _vy, float _t);
